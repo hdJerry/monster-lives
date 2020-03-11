12b4ea8bac85e2c5984591dd86bdb3b26deecbe3
hdjerry <damilolajerryhd@gmail.com>
2020-03-11 21:09:22 +0100

    Updates

---
+++ precache-manifest.9757767bbff18fd846cb65dc6c58bd57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaace8cfe4cbd76305901450a90f321f",
+    "url": "/Monster-React/index.html"
+  },
+  {
+    "revision": "919cfa2d9c4c2e933a0d",
+    "url": "/Monster-React/static/css/main.ff974423.chunk.css"
+  },
+  {
+    "revision": "ac248ffcc0ccc4b2ad23",
+    "url": "/Monster-React/static/js/2.5bab3eca.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monster-React/static/js/2.5bab3eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919cfa2d9c4c2e933a0d",
+    "url": "/Monster-React/static/js/main.27fe720f.chunk.js"
+  },
+  {
+    "revision": "b5842f331d5ec9b8884a",
+    "url": "/Monster-React/static/js/runtime-main.55815ab8.js"
+  }
+]);
